aa5611f03299e190054fb286d17a4a47ecd5fad9
unknown <yufis0844@gmail.com>
2025-07-30 18:14:45 +0300

    Resolved merge conflicts client

--- client/src/store/api.ts
+++ client/src/store/api.ts
@@ -1,8 +1,2 @@
 // export const API = "http://localhost:5000/api"
-<<<<<<< HEAD
-=======
-// export const API = "http://104.251.211.38:5000/api"
-// export const API = "https://104.251.211.38:5000/api"
-// export const API = "https://104.251.211.38/api"
->>>>>>> b8ee2abcd2348429b4c64c04c7fb14dbcd77c4cd
 export const API = "https://api.sevseadivers.com/api"
