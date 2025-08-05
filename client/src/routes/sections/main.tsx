895e6c75392d979fd8143cdcd2cfdd9c2708e799
unknown <yufis0844@gmail.com>
2025-08-05 07:43:02 +0300

    corrected rental path

--- client/src/routes/sections/main.tsx
+++ client/src/routes/sections/main.tsx
@@ -50,7 +50,7 @@ import { AuthGuard } from "../../sections/auth/guard"
 const MyBookingsPage = lazy(
   () => import("../../pages/main/booking/my-bookings")
 )
-const RentalPage = lazy(() => import("../../pages/main/rental"))
+const RentalPage = lazy(() => import("../../pages/main/rental/index"))
 
 // ------------------------------------------------------------------------
 
